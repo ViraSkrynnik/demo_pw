f76f4fae816292f181e32063ecd09efca50c3aa6
Vira Skrynnik <veronskr@gmail.com>
2025-07-09 19:21:09 +0300

    created .env and APP_URL added faker and random username and password added README

---
+++ README.md
@@ -0,0 +1,21 @@
+# Demo Playwright Project
+
+## Setup
+
+1. Clone the repository.
+2. Install dependencies:
+   ```
+   npm install
+   ```
+3. Create a `.env` file in the project root and add:
+   ```
+   APP_URL=https://fe-delivery.tallinn-learning.ee/
+   ```
+
+## Running Tests
+
+To run all tests, use:
+```
+npx playwright test
+```
+
--- test/lesson_16.spec.ts
+++ test/lesson_16.spec.ts
@@ -1,8 +1,12 @@
 import {test, expect, Locator} from "@playwright/test";
+import {faker} from "@faker-js/faker/locale/ar";
+
+const randomUsername = faker.internet.username();
+const randomPassword = faker.internet.password();
 
 test.beforeEach(async ({ page }) => {
-    await page.goto(process.env.BASE_URL);
-})
+    await page.goto(process.env.APP_URL!);
+});
 
 test('sign in button is disabled', async ({ page })=> {
     const loginButton: Locator = page.getByRole('button', { name: 'Sign in' })
@@ -11,4 +15,13 @@ test('sign in button is disabled', async ({ page })=> {
     await userName.fill('test user');
     await password.fill('test');
     await expect(loginButton).toBeDisabled();
+});
+
+test('sign in button is enabled after filling correct data', async ({ page }) => {
+    const loginButton: Locator = page.getByRole('button', { name: 'Sign in' })
+    const userName: Locator = page.locator('#username');
+    const password: Locator = page.locator('#password');
+    await userName.fill(randomUsername);
+    await password.fill(randomPassword);
+    await expect(loginButton).toBeEnabled();
 });
